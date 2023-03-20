bc1a0eb6fb74f05e0c4fc9ae1ff37dacba733167
Leszek <leszek@strixspace.com>
2023-03-20 18:50:45 +0100

    :construction: Working on footer

---
+++ components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="mx-auto max-w-5xl px-4 py-24">
+    <div class="mx-auto max-w-screen-xl">
+      <p class="text-sm text-justify text-left text-gray-600">
+        Informacje zamieszczone na naszej stronie internetowej nie mogą zostać uznane za poradę w jakiejkolwiek sprawie. Treści na niej zawarte mają charakter wyłącznie informacyjny, a my nie ponosimy odpowiedzialności z tytułu powstania jakichkolwiek szkód, wynikających z ich interpretacji. Wszystkie materiały udostępnione na naszej stronie internetowej są aktualne na dzień ich zamieszczenia. Nie możemy jednak zagwarantować, że będą aktualne czy przydatne w każdym stanie faktycznym i w każdym czasie.
+      </p>
+      <ul class="flex flex-wrap justify-left my-6 text-gray-400">
+        <li>
+          <a 
+            href="https://dhosting.pl/pp-leszekkrol" 
+            title="Dumnie zasilany przez dhosting.pl" 
+            data-splitbee-event="Polecam dhosting.pl" 
+            class="underline">Dumnie zasilany przez dhosting.pl</a>
+        </li>
+      </ul>
+      <span class="text-sm text-gray-500 sm:text-center">&copy; 2023 <a href="https://www.leszekkrol.com" title="Leszek Wojciech Król - Strona internetowa" class="hover:underline">Leszek W. Król</a>. Wszelkie prawa zastrzeżone.</span>
+    </div>
+  </footer>
+</template>
