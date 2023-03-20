55f392ae877a00e15dd04fd6b736adc7b20d7fb0
Leszek <leszek@strixspace.com>
2023-03-20 19:42:41 +0100

    :construction: Working on homepage

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-yellow-v1">
-    <div class="w-2/5 mx-auto p-10">
+    <div class="w-2/5 mx-auto p-10 pt-100">
       <h2 class="text-5xl tracking-tight leading-20 font-extrabold text-header">
         Portal ofert pracy dla osób poszukujących lepszej równowagi między życiem zawodowym, a prywatnym.
       </h2>
---
+++ components/navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full z-10 fixed pt-8">
+    <div class="flex items-center p-4 bg-white mx-auto rounded-40 shadow-x-xsm lg:w-5/12">
+      <NuxtLink 
+        to="/faq"
+        title="FAQu"
+        target="_self" 
+        class="btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4">FAQu</NuxtLink>
+      <NuxtLink 
+        to="/o-inicjatywie"
+        title="FAQu"
+        target="_self" 
+        class="btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4">O inicjatywie</NuxtLink>
+    </div>
+  </div>
+</template>
