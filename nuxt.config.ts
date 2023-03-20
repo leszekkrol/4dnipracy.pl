42fc87a1a3fc851fefc5efa5eceb876a6c255929
Leszek <leszek@strixspace.com>
2023-03-20 17:31:15 +0100

    ✨  Added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    '@/assets/main.css',
+  ],
 })
