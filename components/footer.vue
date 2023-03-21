403f4f6b65b3055a971b7e926ba679e48e5cbd6d
Leszek <leszek@strixspace.com>
2023-03-21 05:43:08 +0100

    :construction: Working on footer

--- components/footer.vue
+++ components/footer.vue
@@ -4,13 +4,20 @@
       <p class="text-sm text-justify text-left text-gray-600">
         Informacje zamieszczone na naszej stronie internetowej nie mogą zostać uznane za poradę w jakiejkolwiek sprawie. Treści na niej zawarte mają charakter wyłącznie informacyjny, a my nie ponosimy odpowiedzialności z tytułu powstania jakichkolwiek szkód, wynikających z ich interpretacji. Wszystkie materiały udostępnione na naszej stronie internetowej są aktualne na dzień ich zamieszczenia. Nie możemy jednak zagwarantować, że będą aktualne czy przydatne w każdym stanie faktycznym i w każdym czasie.
       </p>
-      <ul class="flex flex-wrap justify-left my-6 text-gray-400">
+      <ul class="flex flex-wrap items-center gap-x-5 justify-left my-6 text-gray-400">
         <li>
           <a 
-            href="https://github.com/leszekkrol/skutecznyplan.pl" 
+            href="https://github.com/leszekkrol/4dnipracy.pl" 
             title="Rozwijaj projekt wraz ze mną" 
             data-splitbee-event="Rozwijaj projekt Open Source" 
-            class="underline mr-6">Open Source</a>
+            class="underline"><img src="~/assets/open-source.svg" class="h-10" /></a>
+        </li>
+        <li>
+          <a 
+            href="https://github.com/leszekkrol/4dnipracy.pl" 
+            title="Carbon Neutral" 
+            data-splitbee-event="Carbon Neutral" 
+            class="underline"><img src="~/assets/carbon-neutral.svg" class="h-12" /></a>
         </li>
         <li>
           <a 
