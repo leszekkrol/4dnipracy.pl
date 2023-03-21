1b30f4886c27fe00d440f0ce5063257fef9154b4
Leszek <leszek@strixspace.com>
2023-03-21 05:34:02 +0100

    📊 Integrate Splitbee

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "4dnipracy.pl - Twój portal pracy zdalnej",
+      script: [
+        { hid: 'splitbee', src: 'https://cdn.splitbee.io/sb.js', defer: true, 'data-token': '7ICGRZ7AIDXU' }
+      ]
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
