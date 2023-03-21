3473637c9baa5a4835b190828a8662fe6471a74f
Leszek <leszek@strixspace.com>
2023-03-21 21:10:47 +0100

    :construction: Working on offer item

---
+++ components/offer-item.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mt-5 bg-page-theme shadow-xs rounded-16 px-48 py-10">
+    <div class="flex xs:flex-wrap">
+      <div class="text-grey-v1 xs:w-full xs:text-center">Lorem ipsum</div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const { data } = await supabase.from('offers').select('id')
+</script>
+
 <template>
-  <div>
-    
+  <div v-for="item in data">
+    <OfferItem />
   </div>
 </template>
