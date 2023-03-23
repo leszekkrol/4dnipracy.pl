9adf20a7b51165e7813d26633d4c797c94ab72f2
Leszek <leszek@strixspace.com>
2023-03-23 21:27:25 +0100

    📊 Expand Splitbee events

--- components/navigation.vue
+++ components/navigation.vue
@@ -6,6 +6,7 @@
           to="/faq"
           title="4dnipracy.pl - Twój portal pracy zdalnej"
           target="_self" 
+          data-splitbee-event="Homepage" 
           class="relative flex items-center inline-block px-8">
             <img src="~/assets/logo.svg" alt="4dnipracy.pl - Twój portal pracy zdalnej" style="width: 120px">
         </NuxtLink>
@@ -13,11 +14,13 @@
           to="/faq"
           title="FAQu"
           target="_self" 
+          data-splitbee-event="FAQ" 
           class="btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4">FAQu</NuxtLink>
         <NuxtLink 
           to="/o-inicjatywie"
           title="O inicjatywie"
           target="_self" 
+          data-splitbee-event="O inicjatywie" 
           class="btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4">O inicjatywie</NuxtLink>
       </div>
       <div class="flex">
@@ -25,11 +28,13 @@
           to="/wesprzyj"
           title="Wesprzyj"
           target="_self" 
+          data-splitbee-event="Wesprzyj" 
           class="btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4">Wesprzyj</NuxtLink>
         <NuxtLink 
           to="/dodaj-ogloszenie"
           title="Dodaj ogłoszenie"
           target="_self" 
+          data-splitbee-event="Dodaj ogłoszenie" 
           class="btn-yellow-bg btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4">Dodaj ogłoszenie</NuxtLink>
       </div>
     </div>
