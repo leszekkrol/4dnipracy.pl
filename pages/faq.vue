1992d92309619c97f34c88b448052ab48229019a
Leszek <leszek@strixspace.com>
2023-03-23 11:18:57 +0100

    :construction: Working on FAQu

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,8 +1,101 @@
 <template>
-  <div>
-    <h2 class="mb-5 text-3xl md:text-4xl font-bold tracking-tight">FAQu</h2>
-    <p class="leading-30 xs:leading-22">
-      W budowie
-    </p>
-  </div>
+  <section class="px-4 mx-auto max-w-7xl md:px-2">
+    <h1 class="mb-12 text-xl font-bold text-left md:text-3xl md:text-center">Najczęściej zadawane pytania</h1>
+    <div class="flex items-start justify-start mb-12">
+      <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-none w-6 h-6 mr-4 text-gray-700" aria-hidden="true">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div>
+        <p class="mt-0 mb-3 font-semibold text-gray-900">Czym jest praca zdalna?</p>
+        <p class="text-gray-600">
+          Praca zdalna to forma pracy, która umożliwia wykonywanie zadań z dowolnego miejsca, dzięki wykorzystaniu technologii internetowych. Zazwyczaj wymaga korzystania z komputera i dostępu do sieci.
+        </p>
+      </div>
+    </div>
+    <div class="flex items-start justify-start mb-12">
+      <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-none w-6 h-6 mr-4 text-gray-700" aria-hidden="true">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div>
+        <p class="mt-0 mb-3 font-semibold text-gray-900">Czy praca zdalna jest dla każdego?</p>
+        <p class="text-gray-600">
+          Praca zdalna wymaga samodyscypliny, umiejętności planowania czasu i organizacji pracy. Osoby, które potrafią pracować samodzielnie, są w stanie radzić sobie z pracą zdalną.
+        </p>
+      </div>
+    </div>
+    <div class="flex items-start justify-start mb-12">
+      <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-none w-6 h-6 mr-4 text-gray-700" aria-hidden="true">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div>
+        <p class="mt-0 mb-3 font-semibold text-gray-900">Jakie korzyści wynikają z pracy zdalnej?</p>
+        <p class="text-gray-600">
+          Praca zdalna pozwala na większą elastyczność i dostosowanie pracy do własnych potrzeb. Dzięki temu można łatwiej pogodzić pracę z życiem prywatnym. Zmniejsza się również potrzeba dojazdów do pracy, co wpływa pozytywnie na środowisko i oszczędza czas i koszty związane z dojazdem.
+        </p>
+      </div>
+    </div>
+    <div class="flex items-start justify-start mb-12">
+      <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-none w-6 h-6 mr-4 text-gray-700" aria-hidden="true">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div>
+        <p class="mt-0 mb-3 font-semibold text-gray-900">Jakie umiejętności są potrzebne do pracy zdalnej?</p>
+        <p class="text-gray-600">
+          Osoba pracująca zdalnie powinna posiadać dobre umiejętności komunikacyjne, aby móc skutecznie porozumiewać się z zespołem. Ważne są także umiejętności techniczne, ponieważ praca zdalna opiera się na korzystaniu z narzędzi internetowych i oprogramowania.
+        </p>
+      </div>
+    </div>
+    <div class="flex items-start justify-start mb-12">
+      <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-none w-6 h-6 mr-4 text-gray-700" aria-hidden="true">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div>
+        <p class="mt-0 mb-3 font-semibold text-gray-900">Jakie narzędzia są potrzebne do pracy zdalnej?</p>
+        <p class="text-gray-600">
+          Do pracy zdalnej potrzebne są przede wszystkim komputer i szybkie połączenie internetowe. Oprócz tego mogą być potrzebne narzędzia do pracy zespołowej, takie jak komunikator internetowy czy narzędzia do współdzielenia plików.
+        </p>
+      </div>
+    </div>
+    <div class="flex items-start justify-start mb-12">
+      <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-none w-6 h-6 mr-4 text-gray-700" aria-hidden="true">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div>
+        <p class="mt-0 mb-3 font-semibold text-gray-900">Jakie są godziny pracy w przypadku pracy zdalnej?</p>
+        <p class="text-gray-600">
+          Godziny pracy w przypadku pracy zdalnej są zazwyczaj elastyczne i dostosowane do indywidualnych potrzeb pracownika. W niektórych przypadkach może być wymagane dostosowanie się do określonych godzin pracy,
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
