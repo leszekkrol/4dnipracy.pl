73b8c98cc6bad3cb887e060abf48946d8600b95b
Leszek <leszek@strixspace.com>
2023-06-18 11:45:54 +0200

    :construction: Working on offer item

--- components/offer-item.vue
+++ components/offer-item.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="mt-5 bg-page-theme shadow-xs rounded-16 px-48 py-10">
-    <div class="flex xs:flex-wrap">
-      <div class="text-grey-v1 xs:w-full xs:text-center">{{ item.title }}</div>
+  <div class="mt-1 shadow-xs rounded-16 shadow-gray-100 flex flex-col sm:flex-row gap-3 sm:items-center  justify-between px-5 py-4 rounded-md">
+    <div>
+      <span class="text-yellow-800 text-sm">Engineering</span>
+      <h3 class="font-bold mt-px">Senior Full Stack Backend Engineer</h3>
+      <div class="flex items-center gap-3 mt-2">
+         <span class="bg-yellow-100 text-grey-700 rounded-full px-3 py-1 text-sm">Full-time</span>
+         <span class="text-slate-600 text-sm flex gap-1 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+               <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            Remote, UK
+         </span>
+      </div>
+    </div>
+    <div>
+      <button class="btn-yellow-bg btn-hover-bg relative flex items-center font-bold inline-block px-8 py-4 rounded-md flex gap-1 items-center" data-splitbee-event="Aplikuj">
+         Aplikuj
+         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+         </svg>
+      </button>
     </div>
   </div>
+
 </template>
 
 <script>
--- pages/o-inicjatywie.vue
+++ pages/o-inicjatywie.vue
@@ -3,7 +3,7 @@
     <h2 class="mt-20 mb-6 text-4xl text-center font-extrabold leading-80 tracking-normal text-gray-900 md:text-6xl md:tracking-tight"> Dlaczego powinniśmy pracować w 4-dniowym tygodniu pracy?</h2>
 
     <div class="grid gap-16 md:grid-cols-2">
-      <div class="p-4 xs:p-6 md:p-8 bg-gray-100 border border-transparent shadow-md rounded-3xl" v-for="item, index in aboutList">
+      <div class="p-4 xs:p-6 md:p-8 bg-gray-100 border border-transparent rounded-3xl" v-for="item, index in aboutList">
         <h2 class="mb-2 text-3xl md:text-4xl font-bold tracking-tight text-accent">{{ index + 1 }}.</h2>
         <p class="mb-5 font-light text-justify leading-1">
           {{ item.description }}
