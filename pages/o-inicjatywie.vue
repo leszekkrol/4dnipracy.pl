745f97dc57ef55217c82702feabc304e948a382f
Leszek <leszek@strixspace.com>
2023-06-18 11:13:11 +0200

    🚧 Working on about

--- pages/o-inicjatywie.vue
+++ pages/o-inicjatywie.vue
@@ -1,20 +1,26 @@
 <template>
   <section class="px-4 py-24 mx-auto max-w-7xl">
     <h2 class="mt-20 mb-6 text-4xl text-center font-extrabold leading-80 tracking-normal text-gray-900 md:text-6xl md:tracking-tight"> Dlaczego powinniśmy pracować w 4-dniowym tygodniu pracy?</h2>
-    <p>
-      Czy znużyła Cię ta sama stara rutyna, dzień po dniu? Szukasz zmiany, która nie tylko Tobie przyniesie korzyści, ale także światu wokół Ciebie? Nie szukaj dalej niż nasza kampania na rzecz czterodniowego, 32-godzinnego tygodnia pracy bez utraty wynagrodzenia.
-    </p>
-    <p>
-      Tradycyjny harmonogram pracy 9-5, przez 5 dni w tygodniu, jest przestarzały i nie odpowiada już dzisiejszym wymaganiom. Wprowadziliśmy weekend ponad wiek temu i nadszedł czas, aby wprowadzić nowoczesne rozwiązania w kwestii czasu pracy. Dlatego staramy się o nowy sposób pracy, który będzie korzystny dla wszystkich zaangażowanych.
-    </p>
-    <p>
-      Wyobraź sobie, że masz dodatkowy dzień wolny w tygodniu, aby spędzać czas z bliskimi, realizować swoje zainteresowania lub po prostu odpocząć i naładować baterie. Dzięki czterodniowemu tygodniowi pracy będziesz miał więcej czasu na cieszenie się życiem i mniej czasu na stres związany z pracą.
-    </p>
-    <p>
-      Ale korzyści nie kończą się na tym. Pracodawcy zobaczą wzrost produktywności, zmniejszenie absencji oraz bardziej zadowolonych i zaangażowanych pracowników. Gospodarka odnotuje wzrost wydatków, gdyż ludzie będą mieli więcej wolnego czasu, aby cieszyć się życiem. Nasze społeczeństwo stanie się bardziej zrównoważone i sprawiedliwe, gdy ludzie będą mieli więcej czasu na opiekę nad rodziną i udział w życiu społecznym. Nasze środowisko skorzysta z mniejszej emisji węgla i mniejszego czasu spędzonego na dojazdach.
-    </p>
-    <p>
-      Razem możemy stworzyć społeczeństwo, w którym pracujemy, aby żyć, a nie żyjemy, aby pracować. Dołącz do naszej kampanii na rzecz czterodniowego, 32-godzinnego tygodnia pracy bez utraty wynagrodzenia i stwórzmy lepszą przyszłość dla wszystkich.
-    </p>
+
+    <div class="grid gap-16 md:grid-cols-2">
+      <div class="p-4 xs:p-6 md:p-8 bg-gray-100 border border-transparent shadow-md rounded-3xl" v-for="item, index in aboutList">
+        <h2 class="mb-2 text-3xl md:text-4xl font-bold tracking-tight text-accent">{{ index + 1 }}.</h2>
+        <p class="mb-5 font-light text-justify leading-1">
+          {{ item.description }}
+        </p>
+      </div>
+    </div>
+
   </section>
 </template>
+
+<script>
+import { useDataStore } from '~/store/data'
+export default defineComponent({
+  setup() {
+    const dataStore = useDataStore()
+    const aboutList = dataStore.aboutList
+    return { aboutList }
+  },
+})
+</script>
--- store/data.ts
+++ store/data.ts
@@ -5,6 +5,23 @@ export const useDataStore = defineStore({
   id: 'data-store',
   state: () => {
     return {
+      about: [
+        {
+          description: "Czy znużyła Cię ta sama stara rutyna, dzień po dniu? Szukasz zmiany, która nie tylko Tobie przyniesie korzyści, ale także światu wokół Ciebie? Nie szukaj dalej niż nasza kampania na rzecz czterodniowego, 32-godzinnego tygodnia pracy bez utraty wynagrodzenia." 
+        },
+        {
+          description: "Tradycyjny harmonogram pracy 9-5, przez 5 dni w tygodniu, jest przestarzały i nie odpowiada już dzisiejszym wymaganiom. Wprowadziliśmy weekend ponad wiek temu i nadszedł czas, aby wprowadzić nowoczesne rozwiązania w kwestii czasu pracy. Dlatego staramy się o nowy sposób pracy, który będzie korzystny dla wszystkich zaangażowanych." 
+        },
+        {
+          description: "Wyobraź sobie, że masz dodatkowy dzień wolny w tygodniu, aby spędzać czas z bliskimi, realizować swoje zainteresowania lub po prostu odpocząć i naładować baterie. Dzięki czterodniowemu tygodniowi pracy będziesz miał więcej czasu na cieszenie się życiem i mniej czasu na stres związany z pracą." 
+        },
+        {
+          description: "Ale korzyści nie kończą się na tym. Pracodawcy zobaczą wzrost produktywności, zmniejszenie absencji oraz bardziej zadowolonych i zaangażowanych pracowników. Gospodarka odnotuje wzrost wydatków, gdyż ludzie będą mieli więcej wolnego czasu, aby cieszyć się życiem. Nasze społeczeństwo stanie się bardziej zrównoważone i sprawiedliwe, gdy ludzie będą mieli więcej czasu na opiekę nad rodziną i udział w życiu społecznym. Nasze środowisko skorzysta z mniejszej emisji węgla i mniejszego czasu spędzonego na dojazdach." 
+        },
+        {
+          description: "Razem możemy stworzyć społeczeństwo, w którym pracujemy, aby żyć, a nie żyjemy, aby pracować. Dołącz do naszej kampanii na rzecz czterodniowego, 32-godzinnego tygodnia pracy bez utraty wynagrodzenia i stwórzmy lepszą przyszłość dla wszystkich." 
+        }
+      ],
       faq: [
         {
           title: "Czym jest praca zdalna?",
@@ -34,6 +51,7 @@ export const useDataStore = defineStore({
     }
   },
   getters: {
+    aboutList: state => state.about,
     faqList: state => state.faq,
   },
 })
